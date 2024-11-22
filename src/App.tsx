dd0626873c0bfd014097f77c56a0ae065ca6baab
Kaue <757664+kcorrea@users.noreply.github.com>
2024-11-21 19:01:25 -0500

    Downgraded Chakra to V2.5.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from '@chakra-ui/react'
+import { Button, ButtonGroup, Stack } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Button colorScheme='blue'>Button</Button>
+      <Stack spacing={4} direction='row' align='center'>
+        <Button colorScheme='teal' size='xs'>
+          Button
+        </Button>
+        <Button colorScheme='teal' size='sm'>
+          Button
+        </Button>
+        <Button colorScheme='teal' size='md'>
+          Button
+        </Button>
+        <Button colorScheme='teal' size='lg'>
+          Button
+        </Button>
+      </Stack>
     </>
   )
 }
