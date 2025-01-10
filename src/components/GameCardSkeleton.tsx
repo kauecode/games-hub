723ee5e863418accbb7aacf2e8769a49166beaa0
Kaue <757664+kcorrea@users.noreply.github.com>
2025-01-10 12:59:28 -0500

    Fixed overflow bug with skeletons

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card overflow='hidden' variant={'filled'}>
-      <Skeleton height={"150px"} width={"600px"} />
+    <Card width={'100%'} overflow='hidden' variant={'filled'}>
+      <Skeleton height={"150px"} width={'100%'}/>
       <CardBody>
         <SkeletonText height={"100px"} marginBottom={4}/>
       </CardBody>
